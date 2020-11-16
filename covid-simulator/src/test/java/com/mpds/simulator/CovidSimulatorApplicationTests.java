d191061017b0737fcd349d2056ffe51e9e245123
bonfab <fabianbergmann@ymail.com>
2020-11-16 18:56:25 +0100

    initializaion of grid

--- covid-simulator/src/test/java/com/mpds/simulator/CovidSimulatorApplicationTests.java
+++ covid-simulator/src/test/java/com/mpds/simulator/CovidSimulatorApplicationTests.java
@@ -10,4 +10,12 @@ class CovidSimulatorApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    public void testGrid(){
+        Coordinate size = new Coordinate(22, 22);
+        Coordinate binSize = new Coordinate(3, 3);
+        Coordinate overlap = new Coordinate(1, 1);
+        GridBins grid = new GridBins(size, binSize, overlap);
+    }
+
 }
