54167de248f2391c5547dd468e4d77a5f30e4359
bonfab <fabianbergmann@ymail.com>
2020-11-16 10:23:05 +0100

    base classes for simulator

---
+++ covid-simulator/src/main/java/com/mpds/simulator/Person.java
@@ -0,0 +1,16 @@
+package com.mpds.simulator;
+
+public class Person {
+
+    int id;
+    int x;
+    int y;
+    boolean infected;
+    Person(int id){
+        this.id = id;
+        x = 0;
+        y = 0;
+        infected = false;
+    }
+
+}
