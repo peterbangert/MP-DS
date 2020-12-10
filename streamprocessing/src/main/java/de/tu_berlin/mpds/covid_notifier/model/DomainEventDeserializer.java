321a307babae7b6b4e5cd9bc810e60d782f0d149
Peter Bangert <petbangert@gmail.com>
2020-12-10 17:24:10 +0100

    streamprocessing/domaineventserializer: remove logger

--- streamprocessing/src/main/java/de/tu_berlin/mpds/covid_notifier/model/DomainEventDeserializer.java
+++ streamprocessing/src/main/java/de/tu_berlin/mpds/covid_notifier/model/DomainEventDeserializer.java
@@ -7,10 +7,10 @@ import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import lombok.extern.slf4j.Slf4j;
+
 
 import java.io.IOException;
-@Slf4j
+
 public class DomainEventDeserializer extends JsonDeserializer<DomainEvent> {
 
 
@@ -30,7 +30,7 @@ public class DomainEventDeserializer extends JsonDeserializer<DomainEvent> {
                 return codec.treeToValue(tree, PersonHealed.class);
             }
         }catch (Exception ex) {
-           log.error("Could not deserialize the event from Kafka!",ex);
+           System.out.println("Could not deserialize the event from Kafka!" + ex);
            throw ex;
         }
 
