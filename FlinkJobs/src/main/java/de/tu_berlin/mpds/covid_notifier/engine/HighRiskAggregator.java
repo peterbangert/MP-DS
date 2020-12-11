10b1b550833e963cceef7fa8e26aed46740769c9
Sumit <pasumit@cmtclm2y941wjgh.box>
2020-12-11 21:56:23 +0100

    kafka port changed

--- FlinkJobs/src/main/java/de/tu_berlin/mpds/covid_notifier/engine/HighRiskAggregator.java
+++ FlinkJobs/src/main/java/de/tu_berlin/mpds/covid_notifier/engine/HighRiskAggregator.java
@@ -39,8 +39,8 @@ public class HighRiskAggregator {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         Properties properties = new Properties();
-        //Configuration.BOOTSTRAPSERVERADDRESS
-        properties.setProperty("bootstrap.servers", "localhost:9092");
+
+        properties.setProperty("bootstrap.servers", "kafka:9092");
         properties.setProperty("group.id", "covidAnalyser");
         final OutputTag<InfectionReported> outputTag = new OutputTag<InfectionReported>("InfectionReported") {
         };
@@ -52,7 +52,7 @@ public class HighRiskAggregator {
                 Time.of(10, TimeUnit.SECONDS) // delay
         ));
 
-        
+
 
         SimpleModule module = new SimpleModule();
         module.addDeserializer(DomainEvent.class, new DomainEventDeserializer());
@@ -101,58 +101,5 @@ public class HighRiskAggregator {
 
     }
 
-  /*  public static void highRiskContactProducer(StreamExecutionEnvironment env ,Properties properties,ObjectMapper objectMapper) throws Exception {
-        env.setRestartStrategy(RestartStrategies.failureRateRestart(
-                3, // max failures per interval
-                Time.of(5, TimeUnit.MINUTES), //time interval for measuring failure rate
-                Time.of(10, TimeUnit.SECONDS) // delay
-        ));
-        FlinkKafkaConsumer<String> covidSource = new FlinkKafkaConsumer<>(
-                "covid",
-                new SimpleStringSchema(),
-                properties);
-        covidSource.setStartFromEarliest();
-        DataStream<String> covidStream = env.addSource(covidSource);
-
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(DomainEvent.class, new DomainEventDeserializer());
-        objectMapper.registerModule(module);
-        objectMapper.registerModule(new JavaTimeModule());
-
-        final OutputTag<InfectionReported> outputTag = new OutputTag<InfectionReported>("InfectionReported") {
-        };
-
-
-        SingleOutputStreamOperator<PersonContact> contactStream = covidStream
-                .process(new DomainEventSplitter());
-
-
-        DataStream<InfectionReported> infectionsStream = contactStream
-                .getSideOutput(outputTag);
-
-        contactStream
-                .map(data -> {
-                    return Redis.writeContact(
-                            Long.toString(data.getPerson1()),
-                            Long.toString(data.getPerson2()));
-                })
-                .name("Contacts");
-
-        infectionsStream
-                .flatMap(new InfectionRedisMapper())
-                .filter(Objects::nonNull)
-                .addSink(new FlinkKafkaProducer<String>(
-                        "highrisk", new SimpleStringSchema(), properties
-                ))
-                .name("HighRisk");
-
-        infectionsStream.print();
-        contactStream.print();
-
-        env.execute("Covid Engine");
-    }
-
-
-   */
 
 }
