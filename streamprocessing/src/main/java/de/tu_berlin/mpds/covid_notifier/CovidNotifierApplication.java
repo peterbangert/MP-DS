602f04cbdb8f9c16701830553c6fa5060588a256
Peter Bangert <petbangert@gmail.com>
2020-12-11 14:34:08 +0100

    Packagable setup

--- streamprocessing/src/main/java/de/tu_berlin/mpds/covid_notifier/CovidNotifierApplication.java
+++ streamprocessing/src/main/java/de/tu_berlin/mpds/covid_notifier/CovidNotifierApplication.java
@@ -1,5 +1,6 @@
 package de.tu_berlin.mpds.covid_notifier;
 
+import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CovidNotifierApplication {
 
 	public static void main(String[] args) {
+		//BasicConfigurator.configure();
 		SpringApplication.run(CovidNotifierApplication.class, args);
 	}
 
