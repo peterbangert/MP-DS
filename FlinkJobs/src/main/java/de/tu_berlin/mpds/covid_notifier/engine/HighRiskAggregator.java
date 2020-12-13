c1664d4e3dbc23381a05a7783283c4e89208027a
Peter Bangert <petbangert@gmail.com>
2020-12-13 18:24:58 +0100

    FlnkJob: add redis pipeline

--- FlinkJobs/src/main/java/de/tu_berlin/mpds/covid_notifier/engine/HighRiskAggregator.java
+++ FlinkJobs/src/main/java/de/tu_berlin/mpds/covid_notifier/engine/HighRiskAggregator.java
@@ -12,11 +12,14 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.common.time.Time;
+
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.util.Collector;
@@ -42,33 +45,31 @@ public class HighRiskAggregator {
 
         properties.setProperty("bootstrap.servers", "kafka:9092");
         properties.setProperty("group.id", "covidAnalyser");
+
+        properties.setProperty("log.flush.internal.ms", "1000");
+
         final OutputTag<InfectionReported> outputTag = new OutputTag<InfectionReported>("InfectionReported") {
         };
+
+        // Old but maybe relevant code later
         //highRiskContactProducer(env,properties,objectMapper);
+        //SimpleModule module = new SimpleModule();
+        //module.addDeserializer(DomainEvent.class, new DomainEventDeserializer());
+        //objectMapper.registerModule(module);
+        //objectMapper.registerModule(new JavaTimeModule());
 
         env.setRestartStrategy(RestartStrategies.failureRateRestart(
                 3, // max failures per interval
-                Time.of(5, TimeUnit.MINUTES), //time interval for measuring failure rate
-                Time.of(10, TimeUnit.SECONDS) // delay
+                org.apache.flink.api.common.time.Time.of(5, TimeUnit.MINUTES), //time interval for measuring failure rate
+                org.apache.flink.api.common.time.Time.of(10, TimeUnit.SECONDS) // delay
         ));
 
-
-
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(DomainEvent.class, new DomainEventDeserializer());
-        objectMapper.registerModule(module);
-        objectMapper.registerModule(new JavaTimeModule());
-        
         FlinkKafkaConsumer<String> covidSource = new FlinkKafkaConsumer<>(
                 "covid",
                 new SimpleStringSchema(),
                 properties);
         covidSource.setStartFromEarliest();
         DataStream<String> covidStream = env.addSource(covidSource);
-
-        
-
-      //  final OutputTag<InfectionReported> outputTag = new OutputTag<InfectionReported>("InfectionReported") };
 
 
         SingleOutputStreamOperator<PersonContact> contactStream = covidStream
@@ -80,7 +81,7 @@ public class HighRiskAggregator {
 
         contactStream
                 .map(data -> {
-                    return Redis.writeContact(
+                    return PipelineRedis.writeContact(
                             Long.toString(data.getPerson1()),
                             Long.toString(data.getPerson2()));
                 })
@@ -94,8 +95,6 @@ public class HighRiskAggregator {
                 ))
                 .name("HighRisk");
 
-        infectionsStream.print();
-        contactStream.print();
 
         env.execute("HighRiskAggregator");
 
--- FlinkJobs/src/main/java/de/tu_berlin/mpds/covid_notifier/engine/InfectionRedisMapper.java
+++ FlinkJobs/src/main/java/de/tu_berlin/mpds/covid_notifier/engine/InfectionRedisMapper.java
@@ -4,7 +4,10 @@ import de.tu_berlin.mpds.covid_notifier.model.InfectionReported;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.util.Collector;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
 
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 public class InfectionRedisMapper implements FlatMapFunction<InfectionReported, String> {
@@ -12,12 +15,9 @@ public class InfectionRedisMapper implements FlatMapFunction<InfectionReported, 
     @Override
     public void flatMap(InfectionReported data, Collector<String> out) throws Exception {
         try {
-            //Jedis jedis = new Jedis();
-            //Set<String> contacts = jedis.smembers(Long.toString(data.getPersonId()));
-            
-            Set<String> contacts = Redis.getContacts(Long.toString(data.getPersonId()));
+            Set<String> contacts = PipelineRedis.getContacts(Long.toString(data.getPersonId()));
             if (contacts != null) {
-                contacts.stream().forEach(contact -> out.collect(contact));
+                contacts.forEach(contact -> out.collect(contact));
             }
         } catch (Exception e) {
             System.out.println("Infection exception reading data: " + data);
