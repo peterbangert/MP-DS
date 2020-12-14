5ccec6195df036e58b97edd29b1663fc6e8f01ad
Peter Bangert <petbangert@gmail.com>
2020-12-14 16:23:53 +0100

    Revert flink values, remove unused lines

--- streamprocessing/src/main/java/de/tu_berlin/mpds/covid_notifier/CovidNotifierApplication.java
+++ streamprocessing/src/main/java/de/tu_berlin/mpds/covid_notifier/CovidNotifierApplication.java
@@ -1,6 +1,5 @@
 package de.tu_berlin.mpds.covid_notifier;
 
-import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CovidNotifierApplication {
 
 	public static void main(String[] args) {
-		//BasicConfigurator.configure();
 		SpringApplication.run(CovidNotifierApplication.class, args);
 	}
-
 }
